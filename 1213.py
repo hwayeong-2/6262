877ae0d6024719c3dcace6b367b505d099d01a9b
hwayeong-2 <hhhyeong222@gmail.com>
2026-06-02 15:08:51 +0900

    Add files via upload

---
+++ 1213.py
@@ -0,0 +1,7 @@
+grade1 = float(input("1학기 합적 입력 : "))
+grade2 = float(input("2헉기 학점 입력 : "))
+time = int(input("봉사시간 입력 : "))
+
+average = (grade1 + grade2) / 2
+result = (average >= 3.0) and (time >= 10)
+print("장학금 대상 여부 =", result)
